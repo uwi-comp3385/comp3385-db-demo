a7fec6e4b7d95da42e7feb91454eb15d9402154f
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2024-02-14 09:50:02 -0500

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2>Welcome Home</h2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam a reiciendis asperiores explicabo error vero omnis labore libero distinctio dolores. Cum repudiandae itaque eos hic natus numquam necessitatibus reiciendis rerum?</p>
+@endsection
+
+@section('sidebar')
+    <h2>Contact Info</h2>
+    <address>
+    123 Web Dev Way<br />
+    Mona Rd<br />
+    Kingston 7<br />
+    (876) 999-9999
+    </address>
+@endsection
